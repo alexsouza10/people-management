f4f09062c3fd1ebbd2fa1eb5585d261610283420
Alex Souza <alexsouza160298@gmail.com>
2024-05-18 16:57:44 -0300

    feat: address controller

---
+++ src/main/java/com/people/management/project/controller/AddressController.java
@@ -0,0 +1,65 @@
+package com.people.management.project.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.people.management.project.model.Address;
+import com.people.management.project.service.AddressService;
+
+@RestController
+@RequestMapping("api/addresses")
+public class AddressController {
+
+    private final AddressService addressService;
+
+    @Autowired
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Address> createAddress(@RequestBody Address address) {
+        Address createdAddress = addressService.createAddress(address);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAddress);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Address>> getAllAddresses() {
+        List<Address> addresses = addressService.getAllAddresses();
+        return ResponseEntity.ok(addresses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Address> getAddressById(@PathVariable Long id) {
+        Address address = addressService.getAddressById(id);
+        if (address != null) {
+            return ResponseEntity.ok(address);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Address> updateAddress(@PathVariable Long id, @RequestBody Address updateAddress) {
+        Address address = addressService.updateAddress(id, updateAddress);
+        if (address != null) {
+            return ResponseEntity.ok(address);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAddress(@PathVariable Long id) {
+        boolean deleted = addressService.deleteAddress(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
