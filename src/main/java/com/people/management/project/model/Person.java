d0f1e2bbf01852be4925eb3ae8020be739b6a7a4
Alex Souza <alexsouza160298@gmail.com>
2024-05-18 13:12:11 -0300

    feat: add person

---
+++ src/main/java/com/people/management/project/model/Person.java
@@ -0,0 +1,69 @@
+package com.people.management.project.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import jakarta.persistence.*;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private Date dateOfBirth;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+    @OneToOne
+    private Address primaryAddress;
+
+    public Person(Long id, String fullName, Date dateOfBirth, List<Address> addresses, Address primaryAddress) {
+        this.id = id;
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+        this.addresses = addresses;
+        this.primaryAddress = primaryAddress;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public Address getPrimaryAddress() {
+        return primaryAddress;
+    }
+
+    public void setPrimaryAddress(Address primaryAddress) {
+        this.primaryAddress = primaryAddress;
+    }
+
+}
